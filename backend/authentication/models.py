8c8dc3a5b350224d8108c00c00647c449e7d377d
John-Kimani <kimwaweru100@gmail.com>
2023-01-19 16:34:52 +0300

    Func: Create user function

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    '''
+        Class: Overwrites user creation
+    '''
+
+    def create_user(self, email, password, **extra_fields):
+        '''
+            func: Create system users
+        '''
+
+        if not email:
+            raise ValueError(_("An email should be provided!"))
+        
+        email = self.normalize_email(email)
+
+        new_user = self.model(email, **extra_fields)
+
+        new_user.set_password()
+
+        new_user.save()
+
+    

0bf0801907b82f45d307569b4b97e5c1badcde22
John-Kimani <kimwaweru100@gmail.com>
2023-02-08 00:26:45 +0300

    Set up routing

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,7 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import HomePage from "pages/HomePage";
+import RegisterPage from "pages/RegisterPage";
+import LoginPage from "pages/LoginPage";
+import DashboardPage from "pages/DashboardPage";
+
 const App = () => (
-  <div>
-    <h1>My app</h1>
-  </div>
+  <Router>
+    <Routes>
+      <Route path="/" element={ <HomePage />} />
+      <Route path="/dashboard" element={ <DashboardPage />} />
+      <Route path="/login" element={ <LoginPage />} />
+      <Route path="/register" element={ <RegisterPage />} />
+    </Routes>
+  </Router>
 );
 
 export default App;

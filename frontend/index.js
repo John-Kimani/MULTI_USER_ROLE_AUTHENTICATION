12c661f4b58c1f52645f85a41a648e536459729d
John-Kimani <kimwaweru100@gmail.com>
2023-02-07 23:58:02 +0300

    Serve: client application via express

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 
+//configure base directory path using npm module path to get absolute path
+const path = require('path');
+
 const app = express();
 
+/// Serve index.html (serve wild card route)
+app.get('*', (req, res) => {
+    const myPath = path.resolve(__dirname, 'client', 'build', 'index.html')
+    console.log('__dirname', __dirname);
+    console.log('My path: ', myPath);
+    return res.sendFile(myPath);
+});
 
 const PORT = process.env.PORT || 5000;
 

06a4282f890f0df392f5b31c5fb504c6020def63
John-Kimani <kimwaweru100@gmail.com>
2023-02-08 00:20:31 +0300

    Redux and application folder structure

---
+++ frontend/client/src/pages/DashboardPage.js
@@ -0,0 +1,10 @@
+export const DashboardPage = () => {
+  return (
+    <div>
+      <h1>DashboardPage</h1>
+    </div>
+  );
+};
+
+
+export default DashboardPage;
---
+++ frontend/client/src/pages/RegisterPage.js
@@ -0,0 +1,10 @@
+export const RegisterPage = () => {
+    return (
+      <div>
+        <h1>RegisterPage</h1>
+      </div>
+    );
+  };
+  
+  
+  export default RegisterPage;

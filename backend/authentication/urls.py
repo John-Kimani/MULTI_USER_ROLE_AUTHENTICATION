e830b5f51dfbd15b6bbb87f97b17df486710c75e
John-Kimani <kimwaweru100@gmail.com>
2023-02-08 11:49:26 +0300

    Api: Alter endpoint

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.HelloAuthView.as_view(), name='hello_auth'),
-    path('signup/', views.SignUpUserView.as_view(), name='sign_up'),
+    path('register/', views.SignUpUserView.as_view(), name='sign_up'),
     path('login/', views.LoginView.as_view(), name='login')
 ]
--- backend/multi_user_auth/urls.py
+++ backend/multi_user_auth/urls.py
@@ -21,7 +21,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('authentication.urls')),
+    path('api/users/', include('authentication.urls')),
     path('auth/', include('djoser.urls.jwt')),
                           
     #swagger

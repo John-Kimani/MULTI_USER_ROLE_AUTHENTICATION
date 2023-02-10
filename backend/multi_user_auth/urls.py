8af9f1bec7ef0b1ae51678426c6b89b860a9c002
John-Kimani <kimwaweru100@gmail.com>
2023-02-10 18:36:09 +0300

    Feat: Simple JWT Django Rest

--- backend/multi_user_auth/urls.py
+++ backend/multi_user_auth/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 from rest_framework import permissions
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
@@ -22,7 +23,10 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('authentication.urls')),
-    path('auth/', include('djoser.urls.jwt')),
+   #  path('auth/', include('djoser.urls.jwt')),
+   path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view()),
+    path('api/token/verify/', TokenVerifyView.as_view()),
                           
     #swagger
     path('swagger<format>.json.yaml/', schema_view.without_ui(cache_timeout=0), name='schema-json'),

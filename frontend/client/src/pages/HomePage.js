9a1e9c97ce39033e22008be56ad0dd27fcc189bd
John-Kimani <kimwaweru100@gmail.com>
2023-02-10 19:46:18 +0300

    Feat: create Login logic and revamp home page

--- frontend/client/src/pages/HomePage.js
+++ frontend/client/src/pages/HomePage.js
@@ -3,7 +3,8 @@ import Layout from "components/Layout";
 export const HomePage = () => {
     return (
       <Layout title="MultiUser Site | Home" content="Home page">
-        <h1>HomePage</h1>
+        <h1 className="mb-5">MultiUser Site</h1>
+        <p>Welcome to Multi user site!</p>
       </Layout>
     );
   };

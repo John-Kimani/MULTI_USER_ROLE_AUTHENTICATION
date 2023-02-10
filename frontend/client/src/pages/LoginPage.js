1484e034c3e49e1862690e475198c282899e0456
John-Kimani <kimwaweru100@gmail.com>
2023-02-10 17:26:48 +0300

    Apply resetRegistered action on login page

--- frontend/client/src/pages/LoginPage.js
+++ frontend/client/src/pages/LoginPage.js
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { resetRegistered } from "features/user";
 import Layout from "components/Layout";
 
 export const LoginPage = () => {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(resetRegistered());
+  })
     return (
       <Layout title="MultiUser Site | Login" content="Login page">
         <h1>LoginPage</h1>

975ecd0e755070a1eca63a985cf070de7b6dcd25
John-Kimani <kimwaweru100@gmail.com>
2023-02-11 01:05:41 +0300

    Feat: Handle checkAuth on render and cleanup store config

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,24 +1,29 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Provider } from "react-redux";
+import { checkAuth } from "features/user";
 
 import HomePage from "pages/HomePage";
 import RegisterPage from "pages/RegisterPage";
 import LoginPage from "pages/LoginPage";
 import DashboardPage from "pages/DashboardPage";
 
-import { store } from "store";
+const App = () => {
+  const dispatch = useDispatch();
 
-const App = () => (
-  <Provider store={store}>
+  useEffect(() => {
+    dispatch(checkAuth());
+  }, []);
+  return (
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-      </Routes>
-    </Router>
-  </Provider>
-);
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+    </Routes>
+  </Router>
+  );
+  };
 
 export default App;
